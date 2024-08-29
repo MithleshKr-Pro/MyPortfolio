0ae61b6049d9a798c68d5e528794358d499077e3
MithleshKr-Pro <coding4u2@gmail.com>
2024-08-29 23:44:04 +0530

    reUpdated

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-//Practice Codes from Apna College
-for(let i=0;i<=5;i++){
-    sum=sum+i;
-    console.log(sum);
-}
-console.log("SUM=",sum);
+let pop_msg=document.createElement("div");
+let pop_btn=document.createElement("button");
+const body=document.querySelector("body");
+
+// function close_pop(){
+//     close_btn.remove();
+// }
+    setTimeout(()=>{
+        body.prepend(pop_msg);
+        pop_msg.innerHTML=
+        `<div><h2 style="color:darkgoldenrod;">Get Your's Now !</h2>
+        <p id="msg_para" style="letter-spacing:0.1rem">You can just get your's portfolio in Hand.<br>
+        Portfolio will be Ready ASAP.<br>
+        We provide the Best of the market Services.<br>
+        for </p><h3>FREE..!</h3>
+        <button id="yes_btn" >Try now</button>
+        </div>
+        <div>
+        <button class="pop_msg_btn" onclick="close_pop()">X</button>
+        </div>
+        `;
+        pop_msg.classList.add("pop_msg");
+        // pop_btn.classList.add("pop_msg_btn");
+        // pop_msg.innerText="Would You like to have Your's PortFolio Now!!";
+        // pop_btn.innerText="X";
+
+    },4000)
+
+    function close_pop(){
+        pop_msg.remove();
+    }
+
+    const testimonials=document.querySelector("review_box_parent");
+
+    // setTimeout(()=>{
+    //     testimonials.innerHTML=`
+        
+    //     `;
+
+    // },2500)
